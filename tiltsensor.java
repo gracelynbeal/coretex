d450b1db862a326a0f525c8564ea3c81e8f494ef
gracieloo52 <93841190+gracelynbeal@users.noreply.github.com>
2022-06-07 20:25:46 -0400

    Add files via upload

---
+++ tiltsensor.java
@@ -0,0 +1,21 @@
+//Tilt Sensor for physical controller 
+
+int inPin = 7; 
+int value;
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(inPin, INPUT);                // initializes digital pin 7 as input
+}
+
+void loop() 
+{
+  value = digitalRead(inPin); 
+  if(digitalRead(inPin)==1){
+  Serial.write(2);}
+  else{
+  Serial.write(3);           
+  }
+  delay(80);
+}
